45dabed35ac3c0de09cab015afee91261779b297
namnguyen0706 <namk45thue@gmail.com>
2018-07-13 10:54:11 +0700

    Add DBContext 3

---
+++ MyShop.Data/Infrastructure/IDbFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Data.Infrastructure
+{
+    public interface IDbFactory
+    {
+        public interface IDbFactory : IDisposable
+        {
+            MyShopDbContext Init();
+        }
+    }
+}
